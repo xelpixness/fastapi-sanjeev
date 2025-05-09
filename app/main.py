89f0afab5b2ef93d0b1cd7341e33ed8c2c830898
xelpixness <xelpixness@gmail.com>
2025-05-09 22:39:41 +0700

    added new hello msg

--- app/main.py
+++ app/main.py
@@ -23,8 +23,7 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-
 @app.get("/")
 def root():
-    return {"message": "Hello world!"}
+    return {"message": "Hello minik! 👶 ... Blu-u-ue.. Velvet! "}
 
